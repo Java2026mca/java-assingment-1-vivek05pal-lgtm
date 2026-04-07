15f198de153981b958837a528310de87af4187bd
VIVEK PAL <vivek05pal@gmail.com>
2026-04-07 11:21:52 +0530

    Update Main.java

--- src/q01/Main.java
+++ src/q01/Main.java
@@ -5,38 +5,43 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
+        // Upper half
         for (int i = 1; i <= n; i++) {
-          
+            // spaces
             for (int j = 1; j <= n - i; j++) {
                 System.out.print(" ");
             }
 
-       
-            for (int j = 1; j <= (2 * i - 1); j++) {
-                if (j == 1 || j == (2 * i - 1)) {
-                    System.out.print("*");
-                } else {
+            // stars
+            if (i == 1) {
+                System.out.print("*");
+            } else {
+                System.out.print("*");
+                for (int j = 1; j <= 2 * i - 3; j++) {
                     System.out.print(" ");
                 }
+                System.out.print("*");
             }
 
             System.out.println();
         }
 
-      
+        // Lower half
         for (int i = n - 1; i >= 1; i--) {
-           
+            // spaces
             for (int j = 1; j <= n - i; j++) {
                 System.out.print(" ");
             }
 
-     
-            for (int j = 1; j <= (2 * i - 1); j++) {
-                if (j == 1 || j == (2 * i - 1)) {
-                    System.out.print("*");
-                } else {
+            // stars
+            if (i == 1) {
+                System.out.print("*");
+            } else {
+                System.out.print("*");
+                for (int j = 1; j <= 2 * i - 3; j++) {
                     System.out.print(" ");
                 }
+                System.out.print("*");
             }
 
             System.out.println();
