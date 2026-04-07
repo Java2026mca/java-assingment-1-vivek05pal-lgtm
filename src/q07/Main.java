831f0fc9b81da09109f83c120126fbf37f243acb
VIVEK PAL <vivek05pal@gmail.com>
2026-04-07 06:13:56 -0700

    Update Main.java

--- src/q07/Main.java
+++ src/q07/Main.java
@@ -1,14 +1,17 @@
+import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-        int n = sc.nextInt();
+        int n = Integer.parseInt(br.readLine());
         int[] arr = new int[n];
 
+        st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
         int swaps = 0;
@@ -17,7 +20,6 @@ public class Main {
         for (int i = 0; i < n - 1; i++) {
             for (int j = 0; j < n - i - 1; j++) {
                 if (arr[j] > arr[j + 1]) {
-                    // swap
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
@@ -26,13 +28,16 @@ public class Main {
             }
         }
 
-        // Print sorted array
+        // Output
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < n; i++) {
-            System.out.print(arr[i]);
-            if (i < n - 1) System.out.print(" ");
+            sb.append(arr[i]);
+            if (i < n - 1) sb.append(" ");
         }
 
-        System.out.println();
+        System.out.println(sb.toString());
         System.out.println("Swaps: " + swaps);
     }
 }
+               
+       
