035f0addf2535b9f349b70f54cddc86193d653f1
VIVEK PAL <vivek05pal@gmail.com>
2026-04-07 05:46:48 -0700

    Update Main.java

--- src/q03/Main.java
+++ src/q03/Main.java
@@ -5,59 +5,37 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        int[][] arr = new int[n][n];
+        int[][] a = new int[n][n];
+        int top = 0, bottom = n - 1, left = 0, right = n - 1;
+        int val = 1;
 
-        int top = 0, bottom = n - 1;
-        int left = 0, right = n - 1;
-        int num = 1;
-
-        
         while (top <= bottom && left <= right) {
-
-            // Top row
-            for (int i = left; i <= right; i++) {
-                arr[top][i] = num++;
-            }
+            for (int i = left; i <= right; i++) a[top][i] = val++;
             top++;
 
-            
-            for (int i = top; i <= bottom; i++) {
-                arr[i][right] = num++;
-            }
+            for (int i = top; i <= bottom; i++) a[i][right] = val++;
             right--;
 
-            
             if (top <= bottom) {
-                for (int i = right; i >= left; i--) {
-                    arr[bottom][i] = num++;
-                }
+                for (int i = right; i >= left; i--) a[bottom][i] = val++;
                 bottom--;
             }
 
-            
             if (left <= right) {
-                for (int i = bottom; i >= top; i--) {
-                    arr[i][left] = num++;
-                }
+                for (int i = bottom; i >= top; i--) a[i][left] = val++;
                 left++;
             }
         }
 
-        
+        int diag = 0;
         for (int i = 0; i < n; i++) {
+            diag += a[i][i];
             for (int j = 0; j < n; j++) {
-                System.out.print(arr[i][j]);
-                if (j < n - 1) System.out.print(" ");
+                System.out.print(a[i][j] + (j < n - 1 ? " " : ""));
             }
             System.out.println();
         }
 
-        
-        int diagonalSum = 0;
-        for (int i = 0; i < n; i++) {
-            diagonalSum += arr[i][i];
-        }
-
-        System.out.println("Diagonal: " + diagonalSum);
+        System.out.println("Diagonal: " + diag);
     }
 }
