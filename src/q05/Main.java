b68555418752ab2467719093d63734213c7dd64a
VIVEK PAL <vivek05pal@gmail.com>
2026-04-07 11:54:24 +0530

    Update Main.java

--- src/q05/Main.java
+++ src/q05/Main.java
@@ -1,53 +1,30 @@
 import java.util.*;
 
 public class Main {
-    public static void printDiamond(int n) {
-        // Upper half
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n - i; j++) {
-                System.out.print(" ");
-            }
-
-            if (i == 1) {
-                System.out.print("*");
-            } else {
-                System.out.print("*");
-                for (int j = 1; j <= 2 * i - 3; j++) {
-                    System.out.print(" ");
-                }
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-
-        // Lower half
-        for (int i = n - 1; i >= 1; i--) {
-            for (int j = 1; j <= n - i; j++) {
-                System.out.print(" ");
-            }
-
-            if (i == 1) {
-                System.out.print("*");
-            } else {
-                System.out.print("*");
-                for (int j = 1; j <= 2 * i - 3; j++) {
-                    System.out.print(" ");
-                }
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-    }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String shape = sc.next();
 
-        if (shape.equals("diamond")) {
-            int n = sc.nextInt();
-            printDiamond(n);
-        } else {
-            System.out.println("Invalid shape");
+        double area = 0;
+
+        if (shape.equals("circle")) {
+            double r = sc.nextDouble();
+            area = Math.PI * r * r;
+
+        } else if (shape.equals("rectangle")) {
+            double l = sc.nextDouble();
+            double b = sc.nextDouble();
+            area = l * b;
+
+        } else if (shape.equals("triangle")) {
+            double a = sc.nextDouble();
+            double b = sc.nextDouble();
+            double c = sc.nextDouble();
+
+            double s = (a + b + c) / 2.0;
+            area = Math.sqrt(s * (s - a) * (s - b) * (s - c));
         }
+
+        System.out.printf("Area: %.2f", area);
     }
 }
