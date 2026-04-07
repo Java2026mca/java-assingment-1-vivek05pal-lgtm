2d8c5bd63cc78719032925956b7918fdc13c23db
VIVEK PAL <vivek05pal@gmail.com>
2026-04-07 11:42:53 +0530

    Update Main.java

--- src/q06/Main.java
+++ src/q06/Main.java
@@ -8,14 +8,14 @@ public class Main {
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n; j++) {
 
-                // Border condition
+            
                 if (i == 1 || i == n || j == 1 || j == n) {
                     System.out.print("*");
                 } else {
                     System.out.print(" ");
                 }
 
-                // Avoid trailing space
+                
                 if (j < n) System.out.print(" ");
             }
             System.out.println();
